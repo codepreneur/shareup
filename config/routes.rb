273fd4c1dc64e2952a3a66148e9bf91ce4369df2
gomenasaai <contramundee@gmail.com>
2014-07-21 08:56:39 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,30 @@
 Rails.application.routes.draw do
-  resources :shares, except: [:new, :edit]
-  devise_for :users
+
+  get '/dashboard' => 'welcome#dashboard'
+  root to: 'welcome#index'
+
+  namespace :api do
+    resources :shares, except: [:new, :edit]
+  end
+
+  
+
+  scope '/api' do
+    
+    devise_for :users,
+      controllers: {
+        omniauth_callbacks: "users/omniauth_callbacks",
+        registrations: "users/registrations"
+      }
+
+  end
+
+  devise_scope :user do
+    get '/api/current_user' => 'users/sessions#show_current_user', as: 'show_current_user'
+    post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
